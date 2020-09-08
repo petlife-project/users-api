dfe5e3ab15df17d392c7b1e857405a79c26af449
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-09-08 00:05:13 -0300

    Adds MongoDB and COS adapters and adapters factory
    This commit also adds our data model for the two types of users and a custom pylint config file
    
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

---
+++ users/utils/id_generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+
+def generate_id(length=15):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
