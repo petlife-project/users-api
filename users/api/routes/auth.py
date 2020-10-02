1910e97fb9af2ff00b6ba3faa6938f3cbd37e571
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-10-02 01:18:22 -0300

    Adds new authentication route
    This route will be used for loggin in to the webapp and the mobile app
    
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

---
+++ users/api/routes/auth.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+
+class Auth(Resource):
+    """ Route for authenticating the user into the front end apps
+    """
+
+    @staticmethod
+    def post():
+        pass
--- users/app.py
+++ users/app.py
@@ -8,6 +8,7 @@ from cheroot.wsgi import Server as WSGIServer
 
 from users.api.routes.clients import Clients
 from users.api.routes.shops import Shops
+from users.api.routes.auth import Auth
 
 APP = Flask(__name__)
 API = Api(APP)
@@ -18,6 +19,7 @@ SERVER = WSGIServer(('0.0.0.0', PORT), DISPATCHER)
 
 API.add_resource(Clients, '/clients')
 API.add_resource(Shops, '/shops')
+API.add_resource(Auth, '/auth')
 
 if __name__ == '__main__':
     SERVER.safe_start()
