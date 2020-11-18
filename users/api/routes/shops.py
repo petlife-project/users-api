c3f13dd3b9650082c14c00d71c9930d6436ef391
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-11-18 16:08:09 -0300

    Adds service for getting all the shops in the platform
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

--- users/api/routes/shops.py
+++ users/api/routes/shops.py
@@ -3,10 +3,16 @@ from flask_restful import Resource
 from users.api.services.registration import RegistrationService
 from users.api.services.update import UpdateService
 from users.api.services.removal import RemovalService
+from users.api.services.get_all import GetAllService
 
 
 class Shops(Resource):
     """ For registering and updating shops' data."""
+
+    @staticmethod
+    def get():
+        service = GetAllService()
+        return service.get()
 
     @staticmethod
     def post():
